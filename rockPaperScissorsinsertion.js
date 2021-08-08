0a925387c4e0ac2cd8be4a82adc460bdac0da473
Franz Stümpfl <franz.stuempfl@icloud.com>
2021-08-08 17:45:36 +0200

    completed the playRound function with messages and made it case insensitive

--- rockPaperScissorsinsertion.js
+++ rockPaperScissorsinsertion.js
@@ -6,22 +6,41 @@ function computerPlay(i){
     i = getRandomInt(3);
     if (i === 1){
         console.log("Scissors");
-        return "Scissors";
+        return "scissors";
     }
     else if (i === 2){
         console.log("Rock");
-        return "Rock";
+        return "rock";
     }
     else {
         console.log("Paper");
-        return "Paper";
+        return "paper";
     }
 }
 function playRound(playerSelection,computerSelection){
-    console.log("Hello, welcome to RockPaperScissors please enter your choice");
-    playerSelection
+    //console.log("Hello, welcome to RockPaperScissors please enter your choice");
+    playerSelection;
+    if (computerSelection === playerSelection){
+        return "It's a draw!"
+        //draw statement
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return "You lost, Rock beats Scissors!"
+        //Losing 1 PC: Rock, Player: Scissors
+    }
+    else if(computerSelection === "scissors" && playerSelection === "paper"){
+        return "You lost, Scissors beats Paper"
+    }
+    else if(computerSelection === "paper" && playerSelection === "rock"){
+        return "You lost, Paper beats Rock"
+    }
+    else {
+        return "You won!!!"
+    }
 }
-let playerSelection = prompt();
+let playerSelection = prompt().toLowerCase();
 let computerSelection = computerPlay();
+playRound(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection));
 
-computerPlay();
+
