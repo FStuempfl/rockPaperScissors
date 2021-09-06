7c1b3a6873b05ed88ab270bcde8c56e5e46111eb
Franz Stümpfl <franz.stuempfl@icloud.com>
2021-09-06 11:03:07 +0200

    At the moment im cleaning up the functions and when they are invoked, the problem ist to keep track of the global count of games and then tell who won

--- rockPaperScissorsinsertion.js
+++ rockPaperScissorsinsertion.js
@@ -19,7 +19,14 @@ function computerPlay(i){
 }
 
    
-function game(i, count){
+function game(count){
+    for (var count = 0; count < 5; count++){
+        playRound(playerSelection, computerSelection);
+        console.log(count)
+}
+}
+
+/*function game(count){
     for (var count = 0; count <5; count++){
         playRound(playerSelection, computerSelection);
             if (playRound(playerSelection, computerSelection) === "You won!!!"){
@@ -33,47 +40,63 @@ function game(i, count){
         }
 
 }
+*/
+
 function playRound(){
     //console.log("Hello, welcome to RockPaperScissors please enter your choice");
-    playerSelection;
-    if (computerSelection === playerSelection){
-        return "It's a draw!"
+    playerSelection = prompt().toLowerCase();
+    computerSelection = computerPlay().toLowerCase();
+    if (computerSelection == playerSelection){
+        //return "It's a draw!"
         //draw statement
     }
-    else if (computerSelection === "rock" && playerSelection === "scissors") {
-        return "You lost, Rock beats Scissors!"
+    else if (computerSelection == "rock" && playerSelection == "scissors") {
+        computerScore = ++computerScore;
+        console.log(computerScore);
+        //return "You lost, Rock beats Scissors!"
         //Losing 1 PC: Rock, Player: Scissors
     }
-    else if(computerSelection === "scissors" && playerSelection === "paper"){
-        return "You lost, Scissors beats Paper"
+    else if(computerSelection == "scissors" && playerSelection == "paper"){
+        computerScore = ++computerScore;
+        console.log(computerScore);
+        //return "You lost, Scissors beats Paper"
     }
-    else if(computerSelection === "paper" && playerSelection === "rock"){
-        return "You lost, Paper beats Rock"
+    else if(computerSelection == "paper" && playerSelection == "rock"){
+        computerScore = ++computerScore;
+        console.log(computerScore);
+        //return "You lost, Paper beats Rock"
     }
-    else if (computerSelection === "paper" && playerSelection === "scissors"){
-        return "You won!!!"
+    else if (computerSelection == "paper" && playerSelection == "scissors"){
+        playerScore = ++playerScore;
+        console.log(playerScore);
+        //return "You won!!!"
     }
-    else if (computerSelection === "scissors" && playerSelection === "rock"){
-        return "You won!!!"
+    else if (computerSelection == "scissors" && playerSelection == "rock"){
+        playerScore = ++playerScore;
+        console.log(playerScore);
+        //return "You won!!!"
     }
-    else if(computerSelection === "rock" && playerSelection === "paper"){
-        return "You won!!!"
+    else if(computerSelection == "rock" && playerSelection == "paper"){
+        playerScore = ++playerScore;
+        console.log(playerScore);
+        //return "You won!!!"
     }
 
     else{
         return "Please restart and enter a valid value"
     }
 }
-function yourGuess(Guess){
-prompt().toLowerCase()
-}
 
+
+
+let computerScore = 0;
+let playerScore = 0;
 let i = 0;
 let count = 0;
-let playerSelection = yourGuess();
-let computerSelection = computerPlay();
-playRound(playerSelection, computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+let playerSelection;
+let computerSelection;
+//playRound(playerSelection, computerSelection);
+//console.log(playRound(playerSelection, computerSelection));
 game(i);
 
 
